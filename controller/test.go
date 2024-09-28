cef983986a72dee4992837af82c06a13d5e0cb1a
PANURUT SIRINAPAISAN <panurut.siri@kmutt.ac.th>
2024-09-28 10:57:52 +0000

    feat : Swagger UI

--- controller/test.go
+++ controller/test.go
@@ -6,6 +6,13 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// @Summary		Test Controller
+// @Description	Returns a simple message to verify the endpoint.
+// @Tags			Test
+// @Accept			json
+// @Produce		plain
+// @Success		200	{string}	string	"Gu Hum Yaii"
+// @Router			/testrout [get]
 func TestController(c *fiber.Ctx) error {
 	c.Status(fiber.StatusOK).SendString("Gu Hum Yaii")
 	return nil
